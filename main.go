e6621f6b583a0bfbc1bf65581227f185cdfe71dd
0xPeterSatoshi <0xpetersatoshi@gmail.com>
2022-04-07 17:43:35 -0700

    refactor(main): update main script

--- main.go
+++ main.go
@@ -2,51 +2,67 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
 
-	abi "github.com/credmark/abi-sql-view-generator/pkg"
-	_ "github.com/snowflakedb/gosnowflake"
+	"github.com/credmark/abi-sql-view-generator/utils"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	sf "github.com/snowflakedb/gosnowflake"
 )
 
-// const query string = "SELECT DISTINCT CONTRACT_ADDRESS, CONTRACT_NAME, ABI FROM DEPLOYED_CONTRACT_METADATA;"
-const query string = `
-with verified_contracts as (
-    select distinct contract_address, contract_name, abi
-    from deployed_contract_metadata
+var (
+	account   = os.Getenv("SF_ACCOUNT")
+	user      = os.Getenv("SF_USER")
+	password  = os.Getenv("SF_PASSWORD")
+	database  = os.Getenv("SF_DATABASE")
+	schema    = os.Getenv("SF_SCHEMA")
+	warehouse = os.Getenv("SF_WAREHOUSE")
+	role      = os.Getenv("SF_ROLE")
+	namespace = os.Getenv("NAMESPACE")
 )
 
-select
-    l.address as contract_address,
-    c.contract_name as contract_name,
-    c.abi as abi,
-    count(*) as n_logs
-from logs l
-join verified_contracts c on l.address = c.contract_address
-group by 1, 2, 3
-order by 4 desc
-limit 10;
-`
+func getQuery() string {
+	path, _ := filepath.Abs("templates/query_dev.sql")
+	fb, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(fb)
+}
 
 func main() {
-	account := os.Getenv("DBT_SF_ACCOUNT")
-	user := os.Getenv("DBT_SF_USER")
-	password := os.Getenv("DBT_SF_PASSWORD")
-	database := os.Getenv("DBT_SF_DATABASE")
-	schema := os.Getenv("DBT_SF_SCHEMA")
-	warehouse := os.Getenv("DBT_SF_WAREHOUSE")
-	role := os.Getenv("DBT_SF_ROLE")
 
-	connectionString := fmt.Sprintf("%s:%s@%s/%s/%s?warehouse=%s&role=%s", user, password, account, database, schema, warehouse, role)
-	db, err := sql.Open("snowflake", connectionString)
+	cfg := sf.Config{
+		User:      user,
+		Password:  password,
+		Account:   account,
+		Database:  database,
+		Schema:    schema,
+		Warehouse: warehouse,
+		Role:      role,
+	}
+
+	dsn, err := sf.DSN(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	query := getQuery()
+	log.Println("query:", query)
+	log.Printf("connecting to database...\n\tuser=%s\n\trole=%s\n\tdatabase=%s\n\tschema=%s\n\twarehouse=%s\n", user, role, database, schema, warehouse)
+
+	// Open snowflake connection
+	db, err := sql.Open("snowflake", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	log.Println("Connection successful!")
 
+	// Get ABIs and contract addresses
 	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatal(err)
@@ -54,26 +70,25 @@ func main() {
 	defer rows.Close()
 
 	counter := 0
+
+	// Iterate through results, create and submit SQL statements for each ABI and contract address
 	for rows.Next() {
 		var contractAddress string
-		var contractName string
-		var abis []abi.Abi
-		var numLogs int
 		bs := []byte{}
-		err := rows.Scan(&contractAddress, &contractName, &bs, &numLogs)
+		err := rows.Scan(&contractAddress, &bs)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		err = json.Unmarshal(bs, &abis)
+		abi, err := abi.JSON(strings.NewReader(string(bs)))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		for _, v := range abis {
-			a := abi.NewAbi(v, contractAddress)
-			a.CreateSQLFile()
-		}
+		contractAbi := utils.NewAbiContract(contractAddress, abi, namespace)
+		buffer := contractAbi.GenerateSql()
+		log.Println(buffer.String())
+
 		counter += 1
 
 		if counter%100 == 0 {
