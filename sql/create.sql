4babc80186414e3e13d1fafa5e1d19f6fa33fe8c
0xPeterSatoshi <0xpetersatoshi@gmail.com>
2022-04-21 23:10:19 -0700

    fix(sql): fix issues with create.sql

--- sql/create.sql
+++ sql/create.sql
@@ -1,15 +1,12 @@
 with verified_contracts as (
     select distinct contract_address, contract_name, abi
-    from deployed_contract_metadata
+    from ethereum.deployed_contract_metadata
 )
 
 select
     l.address as contract_address,
-    c.contract_name as contract_name,
-    c.abi as abi,
-    count(*) as n_logs
-from logs l
+    c.abi as abi
+from ethereum.logs l
 join verified_contracts c on l.address = c.contract_address
-group by 1, 2, 3
-order by 4 desc
-limit 10;
+group by 1, 2
+having count(*) >= 5;
